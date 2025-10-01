5664376affc011ff1e3f1737f1f7ce2a6071a1e0
Harold1Maldonado <haroldmaldonado1@gmail.com>
2025-10-01 19:27:06 +0000

    se crean funciones de conversion

--- app.js
+++ app.js
@@ -12,3 +12,24 @@ console.log(sum(7,3))
 // Exporta la función para usarla en otros archivos 
 // (similar a la palabra clave "export" cuando se usa webpack)
 module.exports = { sum };
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+function fromDollarToYen() => {
+    
+
+}
+
+function fromEuroToDollar() => {
+
+}
+
+function fromYenToPound() => {
+
+}
+
